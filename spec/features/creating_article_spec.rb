61cd1f08c039f8089119ec7292449c5db8095504
Adam Morgan <aymorgan@hotmail.co.uk>
2017-08-13 17:44:26 +0100

    Create article with passing test

---
+++ spec/features/creating_article_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "Creating articles" do
+  scenario "A user creates a new article" do
+    visit '/'
+
+    click_link "New Article"
+    fill_in "Title", with: "Creating first artile"
+    fill_in "Body", with: "Lorum Ipsum"
+    click_button "Create Article"
+
+    expect(page).to have_content("Article has been created")
+    expect(page.current_path).to eq(articles_path)
+  end
+end
